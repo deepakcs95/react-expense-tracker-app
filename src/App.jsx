dc0a5c7885382cc1e53524972b29fe14e8b05058
deepak_c_s <deepak@google.com>
2023-05-31 10:33:51 +0530

    mvp done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Expense from "./components/Expense.jsx";
 import Balance from "./components/Balance.jsx";
 import Total from "./components/Total.jsx";
 import AddTransaction from "./components/AddTransaction.jsx";
+import TransactionList from "./components/TransactionList.jsx";
 // import dotenv from "dotenv";
 
 function App() {
@@ -19,18 +20,19 @@ function App() {
     date: today.toLocaleDateString(),
     description: "Monthly salary",
   };
-  const inputRef = useRef(null);
 
+  const inputRef = useRef(null);
   const categoryRef = useRef("Expense");
   const [data, setData] = useState(Data);
-  console.log(data);
 
-  function hanleSubmit(e) {
-    e.preventDefault();
+  console.log(data.transactions[data.transactions.length - 1]);
+
+  function hanleSubmit(type) {
     const amount = Number(inputRef.current.value);
     if (amount && amount > 0) {
       newTransaction.category = categoryRef.current;
       newTransaction.amount = amount;
+      newTransaction.type = type;
       // console.log(newTransaction);
       setData({
         ...data,
@@ -40,28 +42,30 @@ function App() {
   }
 
   function handleCategoryChange(e) {
+    console.log(typeof e.target);
     categoryRef.current = e.target.value;
     console.log(categoryRef.current);
   }
 
   return (
-    <>
-      <section className="card">
-        <h1>expense tracker</h1>
-        <div className="transaction-card">
-          <Income data={data} />
-          <Expense data={data} />
-          <Balance data={data} />
-          <Total data={data} />
-        </div>
+    <section className="card">
+      <h1>expense tracker</h1>
+      <div className="transaction-card">
+        <Income data={data} />
+        <Expense data={data} />
+        <Balance data={data} />
+        <Total data={data} />
+      </div>
 
-        <AddTransaction
-          ref={inputRef}
-          oncategoryChange={handleCategoryChange}
-          onSubmit={hanleSubmit}
-        />
-      </section>
-    </>
+      <AddTransaction
+        type={data.categories}
+        ref={inputRef}
+        oncategoryChange={handleCategoryChange}
+        onSubmit={hanleSubmit}
+      />
+
+      <TransactionList list={data.transactions} />
+    </section>
   );
 }
 
